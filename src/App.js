286eaa05241e625c6a16dac490442ef875770947
Robson Vinicius <robsondgn@outlook.com>
2020-07-28 02:56:16 -0300

    Criando o componente carrousel e alterando os vídeos no data

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import Menu from './components/Menu';
 import data from './data/dados_iniciais.json';
 import Banner from './components/BannerMain';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
-    <div>
+    <div className="BackgroundApp">
       <Menu />      
       <Banner
         videoTitle={data.categorias[0].videos[0].titulo}
         url={data.categorias[0].videos[0].url}
         videoDescription={"Após a Guerra Civil, um jovem prodígio nas artes marciais, recém chegado da China, acaba envolvido nas sangrentas disputas entre as famílias líderes do crime organizado chinês."}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={data.categorias[0]}
+      />
+
+      <Carousel        
+        category={data.categorias[1]}
+      />
+      
+      <Carousel        
+        category={data.categorias[2]}
+      />
+
+      <Carousel        
+        category={data.categorias[3]}
+      />
+
+      <Carousel        
+        category={data.categorias[4]}
+      />
+
+      <Carousel        
+        category={data.categorias[5]}
       />
     </div>
   );
