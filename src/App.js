6d0899ed1d1c8798c0dee6dd863d31481c05b5c8
Robson Vinicius <robsondgn@outlook.com>
2020-07-28 01:43:07 -0300

    importação do data e criação do BannerMain

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Menu from './components/Menu';
+import data from './data/dados_iniciais.json';
+import Banner from './components/BannerMain';
 
 function App() {
   return (
     <div>
-      <Menu />
-      
+      <Menu />      
+      <Banner
+        videoTitle={data.categorias[0].videos[0].titulo}
+        url={data.categorias[0].videos[0].url}
+        videoDescription={"Após a Guerra Civil, um jovem prodígio nas artes marciais, recém chegado da China, acaba envolvido nas sangrentas disputas entre as famílias líderes do crime organizado chinês."}
+      />
     </div>
   );
 }
