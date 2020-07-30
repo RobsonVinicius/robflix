20f2cf641faf75d0c6cfb3bafcf631774d54a30d
Robson Vinicius <robsondgn@outlook.com>
2020-07-30 16:26:31 -0300

    Deixando o carousel infinito

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -13,14 +13,14 @@ const Container = styled.ul`
       margin: auto;
       width: 30px;
       height: 30px;
-      transform: initial;
+      transform: initial;      
       &:before {
         font-size: 30px;
       }
     }
 
     .slick-prev {
-      left: 0;
+      left: 16px;            
     }
 
     .slick-next {
@@ -29,12 +29,12 @@ const Container = styled.ul`
 `;
 
 export const SliderItem = styled.li`
-  margin-right: 16px;
+  margin-right: 16px;  
   img {
     margin: 16px;
     width: 298px;
-    height: 197px;
-    object-fit: cover;
+    height: 197px;    
+    object-fit: cover;    
   }
 `;
 
@@ -42,11 +42,11 @@ const Slider = ({ children }) => (
   <Container> 
     <SlickSlider {...{
       dots: false,
-      infinite: false,
+      infinite: true,
       speed: 300,
       centerMode: false,
       variableWidth: true,
-      adaptiveHeight: true,
+      adaptiveHeight: true,         
     }}
     >
       { children }
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
-  return (
-    <>
-      <PageDefault>
-        <h1>Cadastro Categoria</h1>
-        <form>
-          <label>
-            Nome da Categoria:
-            <input type="text"></input>
-          </label>
-        </form>
-        <button>Cadastrar</button>
-        <Link to="/">
-          Ir para a home
-        </Link>
-      </PageDefault>
-    </>
+  const [categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');  
+  return (    
+    <PageDefault>
+      <h1>Cadastro Categoria: {nomeDaCategoria}</h1>
+      <form>
+        <label>
+          Nome da Categoria:
+          <input 
+            type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandlerQueErroPediu(infosDoEvento) {
+              setNomeDaCategoria(infosDoEvento.target.value);
+            }}
+            ></input>
+          
+        </label>
+      </form>
+
+      <ul>
+        {categorias.map((categoria) => {
+          return (
+            <li key="categoria">
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
+
+      <button>Cadastrar</button>
+      <Link to="/">
+        Ir para a home
+      </Link>
+    </PageDefault>    
   )
 }
 
